2158bbf690404d88382246ee779b839dd51ba2a9
Alexander96 <alexanders96r.s@gmail.com>
2021-07-14 04:44:45 -0500

    Se agrego contenido JavaScript (VERS. 1.0)

--- Proyecto Web Git/js/main.js
+++ Proyecto Web Git/js/main.js
@@ -1,1 +1,24 @@
 // Empezarea a realizar los cambios con JS
+
+// Generando modal btn comprar
+let btnComp = document.getElementById("btn-buy");
+let inputCant = document.getElementById("txtcant");
+
+btnComp.addEventListener("click", () => {
+    if (inputCant.value > 0) {
+        alert("Se agrego su compra exitosamente");
+    } else {
+        alert("Error!!! Compra minima 1 boleto");
+    }
+});
+
+// boton incrementar
+function increment() {
+    let num = inputCant.value;
+
+
+}
+
+function decrement () {
+    let num = inputCant.value;
+}
