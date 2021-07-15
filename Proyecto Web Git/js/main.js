02ea5cb85dd400557ed82e6aeb6a38d5bc936b4b
Alexander96 <alexanders96r.s@gmail.com>
2021-07-14 21:02:02 -0500

    Se agrego contenido JavaScript (VERS. 2.0)

--- Proyecto Web Git/js/main.js
+++ Proyecto Web Git/js/main.js
@@ -1,7 +1,7 @@
 // Empezarea a realizar los cambios con JS
 
 // Generando modal btn comprar
-let btnComp = document.getElementById("btn-buy");
+let btnComp = document.getElementById("buy1");
 let inputCant = document.getElementById("txtcant");
 
 btnComp.addEventListener("click", () => {
@@ -12,13 +12,23 @@ btnComp.addEventListener("click", () => {
     }
 });
 
-// boton incrementar
-function increment() {
-    let num = inputCant.value;
+//Creando funciones para botones
+// btn-incremento
+let btnIncrement =  document.getElementById("increment");
+btnIncrement.addEventListener("click", () => {
+    if (inputCant.value >= 0) {
+        inputCant.value++;
+        inputCant.innerText = inputCant.value;
+    }
+});
+// btn-decremento
+let btnDecremenr = document.getElementById("decrement");
+btnDecremenr.addEventListener("click", () => {
+    if (inputCant.value > 0) {
+        inputCant.value--;
+        inputCant.innerText = inputCant.value;
+    } else {
+        alert("No se puede disminuir a menos de 0");
+    }
+});
 
-
-}
-
-function decrement () {
-    let num = inputCant.value;
-}
